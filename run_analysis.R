367de229e7f9e6f0e36f2dba502ce44df73472f4
MenghaoLiu <menghao1201@pku.edu.cn>
2015-03-17 23:02:36 +0800

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,41 @@
-setwd("getdataproject")
-subject_test <- read.table("subject_test.txt")
-y_test <- read.table("y_test.txt")
-x_test <- read.table("x_test.txt")
+## Check whether the required raw data set
+## has been unzipped in the working directory
+if(!file.exists("UCI HAR Dataset")) {
+    stop("can not find the raw data folder")
+}
+
+## Read the test data and merge the columns generating the testDT data frame 
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+x_test <- read.table("./UCI HAR Dataset/test/x_test.txt")
 testDT <- cbind(subject_test, y_test, x_test)
-subject_train <- read.table("subject_train.txt")
-y_train <- read.table("y_train.txt")
-x_train <- read.table("x_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+x_train <- read.table("./UCI HAR Dataset/train/x_train.txt")
 trainDT <- cbind(subject_train, y_train, x_train)
-features <- read.table("features.txt", stringsAsFactors = F)
+features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = F)
 names(features) <- c("ID", "fea")
 names(testDT) <- c("subject", "num", features$fea)
 names(trainDT) <- c("subject", "num", features$fea)
 mergeDT1 <- rbind(trainDT, testDT)
 n <- grep("(mean|std)\\(\\)", colnames(mergeDT1))
 mergeDT2 <- subset(mergeDT1, select = c(1, 2, n))
-activity_labels <- read.table("activity_labels.txt", stringsAsFactors = F)
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors = F)
 names(activity_labels) <- c("num", "activity")
 mergeDT3 <- merge(mergeDT2, activity_labels, by = "num", sort = F)
 mergeDT3$activity <- factor(mergeDT3$activity, order = T, levels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
 mergeDT3 <- mergeDT3[order(mergeDT3$subject, mergeDT3$num), ]
 mergeDT <- subset(mergeDT3, select = c(subject, activity, 3:68))
+
+if(!is.element("reshape2", installed.packages()[,1])) {
+    print("Installing packages")
+    install.packages("reshape2")
+}
 library(reshape2)
+
 m <- colnames(mergeDT)
 meltDT <- melt(mergeDT, id = c("subject", "activity"), measure.vars = m[3:68])
 meanDT <- dcast(meltDT, subject + activity ~ variable, mean)
 data <- write.table(meanDT, "finalData.txt", row.names = F, col.names = T)
 data <- read.table("finalData.txt", header = T)
+ 
